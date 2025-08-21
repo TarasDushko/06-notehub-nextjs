aa8a3404b2500f643ddbfe933dbffe2490861174
Taras <tarasdushko@gmail.com>
2025-08-21 19:25:27 +0200

    f

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -3,16 +3,15 @@ import { fetchNoteById } from "@/lib/api";
 import { QueryClient, dehydrate } from "@tanstack/react-query";
 import NoteDetailsClient from "./NoteDetails.client";
 
-interface NotePageProps {
+interface PageProps {
   params: { id: string };
 }
 
-const NoteDetails = async ({ params }: NotePageProps) => {
+const NoteDetails = async ({ params }: PageProps) => {
   const { id } = params;
 
   const queryClient = new QueryClient();
 
-  // Завантажуємо нотатку на сервері
   await queryClient.prefetchQuery({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
