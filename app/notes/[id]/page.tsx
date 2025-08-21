df5d2ba3d0e15c1f3964dc5b4ba380d9c6703505
Taras <tarasdushko@gmail.com>
2025-08-21 19:54:34 +0200

    fifi

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,11 +2,8 @@ import { fetchNoteById } from "@/lib/api";
 import { QueryClient, dehydrate } from "@tanstack/react-query";
 import NoteDetailsClient from "./NoteDetails.client";
 
-interface NotePageProps {
-  params: { id: string };
-}
-
-const NoteDetails = async ({ params }: NotePageProps) => {
+const NoteDetails = async (props: unknown) => {
+  const params = (props as { params: { id: string } }).params;
   const { id } = params;
 
   const queryClient = new QueryClient();
